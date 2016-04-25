53a0ed3c75b52f0455f0ce379a2498c8c3af336b
dnuzum <derek.nuzum@gmail.com>
2016-04-25 11:17:08 -0700

    commit fixes

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
+var multer = require('multer');
+var cloudinary = require('cloudinary');
 var app = express();
+
+var upload = multer({dest: './uploads'});
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.post("/", upload.single('myfile'), function(req, res) {
+	// res.send(req.file);
+	cloudinary.uploader.upload(req.file.path, function(result) {
+		res.send(result);
+	});
 });
 
 app.listen(3000);
